547b0bb27b9efcbd395a4a49bd309f40ba286dc0
Vorleak Yek <vorleakyek@gmail.com>
2024-11-27 22:18:47 -0800

    fetch data from sanity

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import {client} from '@/sanity/lib/client';
 
 export default async function Home( {searchParams}: {searchParams: Promise<{query?: string}>}) {
 
   const query = (await searchParams).query;
 
-  const posts = [
-    {
-      _createdAt: new Date(),
-      views: 55,
-      author: { _id: 1, name:"VLKY"},
-      _id: 1,
-      description: "This is a description",
-      image: "https://media.newyorker.com/photos/65527dfc8891bf3f2830ffd7/master/w_960,c_limit/Robin-Pandas-Leave.jpg",
-      category: "Zoo",
-      title: "Panda Zoo"
-    },
-  ]
+  const posts = await client.fetch(STARTUPS_QUERY);
+
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     views: 55,
+  //     author: { _id: 1, name:"VLKY"},
+  //     _id: 1,
+  //     description: "This is a description",
+  //     image: "https://media.newyorker.com/photos/65527dfc8891bf3f2830ffd7/master/w_960,c_limit/Robin-Pandas-Leave.jpg",
+  //     category: "Zoo",
+  //     title: "Panda Zoo"
+  //   },
+  // ]
 
   return (
     <>
